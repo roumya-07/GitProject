c1db428e981572ef9d21cb695b623a2309a18e2c
Arun <mailtomakarun@gmail.com>
2022-12-12 15:47:51 +0530

    Mak

---
+++ GitProject/Models/Mak.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitProject.Models
+{
+    public class Mak
+    {
+        public string Arun { get; set; } = null;
+    }
+}
