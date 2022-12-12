34570a3dc1bcd3970f348112614b715cfc50586c
adesh.swain <adesh.swain@CSMBHUL932>
2022-12-12 15:28:33 +0530

    Iservice

---
+++ GitProject/Service/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GitProject.Models;
+
+namespace GitProject.Service
+{
+   public interface IService
+    {
+        Task<List<DotNet>> GetAll();
+        Task<DotNet> GetOne(int ID);
+        Task<int> Insert(DotNet Dt);
+        Task<int>Update(DotNet Dt);
+        Task<int> Delete(int ID);
+    }
+}
