44485a8295d8d837d0a34c8c689be93251954b75
shyamsundar.baral <shyamsundar.baral@CSMBHUL1027>
2022-12-12 16:14:59 +0530

    StudentReposityory

---
+++ GitProject/Repository/StudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitProject.Repository
+{
+    public class StudentRepository
+    {
+    }
+}
