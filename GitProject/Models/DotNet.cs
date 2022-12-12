f89047d3cfd34983c9e06a703f6762b51baddf6b
adesh.swain <adesh.swain@CSMBHUL932>
2022-12-12 13:27:46 +0530

    Model Class

---
+++ GitProject/Models/DotNet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitProject.Models
+{
+    public class DotNet
+    {
+        public int ID { get; set; } = 0;
+        public string Name { get; set; } = null;
+        public string Phone { get; set; } = null;
+        public string Address { get; set; } = null;
+    }
+}
