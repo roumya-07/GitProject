64d312d047cda94d37ad79f16c2d43198119218e
ShuvashreeShuvadarsiniPradhan <120356413+ShuvashreeShuvadarsiniPradhan@users.noreply.github.com>
2022-12-12 16:41:44 +0530

    Service

---
+++ GitProject/Service/Service.cs
@@ -0,0 +1,36 @@
+﻿using GitProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitProject.Service
+{
+    public class Service : IService
+    {
+        public Task<int> Delete(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<DotNet>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DotNet> GetOne(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Insert(DotNet Dt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update(DotNet Dt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
