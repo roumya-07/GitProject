253e6b468a47e9244ed99b72bd7fdaead1f7f7b8
Roumya Ranjan Behera <95536262+roumya-07@users.noreply.github.com>
2022-12-12 12:54:28 +0530

    DataBase

---
+++ Git.sql
@@ -0,0 +1,28 @@
+Create Table NewDotNet
+(
+ID int primary key,
+Name Varchar(100),
+Phone Varchar(12),
+Address Varchar(250)
+);
+
+
+Create Procedure SP_DotNet
+(
+@ID int = 0,
+@Name Varchar(100) = null,
+@Phone Varchar(12) = null,
+@Address Varchar(250) = null,
+@Action Varchar(100)
+)
+as
+begin
+if(@Action = 'Insert')
+Insert Into NewDotNet values(@ID,@Name,@Phone,@Address)
+else if(@Action = 'Update')
+Update NewDotNet Set ID=@ID,Name=@Name,Phone=@Phone,Address=@Address
+else if(@Action='Delete')
+delete from  NewDotNet where ID=@ID
+else if(@Action='All')
+Select * From NewDotNet
+end
