05d09e2e6fe7653edb9acd0f42a77baf189790aa
Arun <mailtomakarun@gmail.com>
2022-12-12 17:15:10 +0530

    statup

--- GitProject/Startup.cs
+++ GitProject/Startup.cs
@@ -46,7 +46,7 @@ namespace GitProject
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Git}/{action=Index}/{id?}");
             });
         }
     }
