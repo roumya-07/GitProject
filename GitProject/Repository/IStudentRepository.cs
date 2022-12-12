721c867cadc555e4de526f973e838278370a97e0
nayaksubha <nayaksubhasmita270@gmailcom>
2022-12-12 16:31:01 +0530

    IStudentRepository

---
+++ GitProject/Repository/IStudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GitProject.Models;
+
+namespace GitProject.Repository
+{
+   public interface IStudentRepository
+    {
+        Task<List<DotNet>> GetAll();
+        Task<DotNet> GetByid(int pid);
+        Task<int> Insert(DotNet prd);
+        Task<int> Update(DotNet prd);
+        Task<int> Delete(int id);
+
+    }
+}
